8ae7852c277706e9d18e5f9483226acf86eef14e
RohanAcharV <rohanacharv@gmail.com>
2024-01-19 12:18:20 +0530

    cam feature

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import CustomWebcam from './CustomWebcam';
+
+function App() {
+  return (
+    <div className="App">
+     <CustomWebcam/>
+    </div>
+  );
+}
+
+export default App;
